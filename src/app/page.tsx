3bb6353c1483206d49ae1b86f0a420f37e9005a0
renansui <renanddtao@gmail.com>
2023-06-24 14:09:07 -0300

    fixed page.tsx pattern and blur occupying space below screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,15 @@ export default function Home() {
 
   return (
     <>
-      <main className="h-screen w-screen bg-black text-zinc-100">
+      <main className="relative h-screen w-screen bg-black text-zinc-100">
         {showBootScreen && <BootScreen {...{ setShowLoading }} />}
         {showLoading && <LoadingScreen {...{ setShowBootScreen }} />}
-        <div // z-10
-          className="absolute z-10 h-full w-full bg-[url(/images/pattern.png)] bg-[length:15px_15px] bg-repeat opacity-30"
+        {/* <div // z-10
+          className="absolute bottom-0 left-0 right-0 top-0 z-10 bg-[url(/images/pattern.png)] bg-[length:15px_15px] bg-repeat opacity-30"
         />
         <div // z-10
-          className="absolute z-10 h-full w-full bg-[radial-gradient(circle,_hsla(0,_0%,_100%,_0.05)_0%,_hsla(0,_0%,_0%,_0.5)_100%)]"
-        />
+          className="absolute bottom-0 left-0 right-0 top-0 z-10 bg-[radial-gradient(circle,_hsla(0,_0%,_100%,_0.05)_0%,_hsla(0,_0%,_0%,_0.5)_100%)]"
+        /> */}
       </main>
     </>
   )
