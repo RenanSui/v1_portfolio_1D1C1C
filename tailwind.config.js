10ed911dbfbeec0d4a255a0cb4bd856a4c0ce7b7
renansui <renanddtao@gmail.com>
2023-06-24 13:54:24 -0300

    added more colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,19 @@ module.exports = {
       colors: {
         nier: {
           100: '#FAF8EF',
-          300: '#93928B',
+          300: '##BCAEA3',
+          // 300: '#93928B',
+          200: '#A19D93',
           400: '#A6A188',
           800: '#302A24',
           900: '#31312F',
+          950: '#191713',
         },
       },
       fontFamily: {
         Concielian: ['Concielian', 'sans-serif'],
+        RodinProM: ['RodinProM', 'sans-serif'],
+        RodinProDB: ['RodinProDB', 'sans-serif'],
       },
     },
   },
