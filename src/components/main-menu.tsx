d6be6c6ad85b4ea859cc14827e0ec2e3445c58c5
renansui <renanddtao@gmail.com>
2023-07-06 13:04:44 -0300

    replaced motion.div to ShellAnimated component

--- src/components/main-menu.tsx
+++ src/components/main-menu.tsx
@@ -1,6 +1,7 @@
 import { RodinPro } from '@/lib/fonts'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { useState } from 'react'
+import { ShellAnimated } from './ui/ShellAnimated'
 import { MenuOption } from './ui/menu-option'
 import { MenuOptions } from './ui/menu-options'
 
@@ -19,12 +20,8 @@ const MainMenu = () => {
   }
 
   return (
-    <motion.div
-      className={`flex h-full w-full flex-col bg-nier-800 ${RodinPro.className} font-medium tracking-[0.15em]`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 0.6, duration: 1 } }}
-      exit={{ opacity: 0, transition: { duration: 0.3 } }}
-      // animate={{ opacity: 1, transition: { delay: 1, duration: 3 } }}
+    <ShellAnimated
+      className={`flex h-full w-full flex-col bg-nier-800 font-medium tracking-[0.15em] ${RodinPro.className}`}
     >
       {/* <div className="h-full w-full">
         <div className="stars1 absolute h-full w-full bg-[url(/images/stars-3.jpg)] bg-cover opacity-30" />
@@ -34,26 +31,16 @@ const MainMenu = () => {
 
       <AnimatePresence>
         {showPressAny && (
-          <motion.div
-            className="absolute bottom-48 left-1/2 -translate-x-1/2 sm:bottom-60"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 1, delay: 0.6 } }}
-            exit={{ opacity: 0, transition: { duration: 0.3 } }}
-          >
+          <ShellAnimated className="absolute bottom-48 left-1/2 -translate-x-1/2 sm:bottom-60">
             <MenuOptions>
               <MenuOption onClick={HandlePressAny}>Press Any Button</MenuOption>
             </MenuOptions>
-          </motion.div>
+          </ShellAnimated>
         )}
       </AnimatePresence>
       <AnimatePresence>
         {showMenuOptions && (
-          <motion.div
-            className="absolute bottom-20 left-1/2 -translate-x-1/2 sm:bottom-40"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 1, delay: 0.6 } }}
-            exit={{ opacity: 0, transition: { duration: 0.3 } }}
-          >
+          <ShellAnimated className="absolute bottom-20 left-1/2 -translate-x-1/2 sm:bottom-40">
             <MenuOptions className="flex flex-col gap-3">
               <MenuOption>Continue</MenuOption>
               <MenuOption>New Game</MenuOption>
@@ -61,10 +48,10 @@ const MainMenu = () => {
               <MenuOption>License</MenuOption>
               <MenuOption onClick={HandleExitGame}>Exit Game</MenuOption>
             </MenuOptions>
-          </motion.div>
+          </ShellAnimated>
         )}
       </AnimatePresence>
-    </motion.div>
+    </ShellAnimated>
   )
 }
 
