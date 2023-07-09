4e3dca57f9a5853c192ced8fc3ac96f20492b7f2
renansui <renanddtao@gmail.com>
2023-07-09 12:41:11 -0300

    add showLine condition to menu-option

--- src/components/ui/menu-option.tsx
+++ src/components/ui/menu-option.tsx
@@ -7,12 +7,14 @@ import { FC, HTMLAttributes, ReactNode, memo, useRef } from 'react'
 interface MenuOptionsProps extends HTMLAttributes<HTMLDivElement> {
   children?: ReactNode
   textHidden: string
+  showLine?: boolean
 }
 
 const MenuOptionComponent: FC<MenuOptionsProps> = ({
   children,
   textHidden,
   className,
+  showLine,
   ...props
 }) => {
   const { word, start } = useTypingText(children as string, 20)
@@ -37,16 +39,27 @@ const MenuOptionComponent: FC<MenuOptionsProps> = ({
     >
       <h1
         ref={TextRef}
-        className={'select-none text-lg text-nier-100 md:text-xl'}
+        className={
+          'pointer-events-none select-none text-lg text-nier-100 md:text-xl'
+        }
         data-value={word}
         data-hidden={textHidden}
       >
         {word}
       </h1>
-      <div className="MenuLine flex items-center justify-center gap-1">
-        <span className="dot h-[6px] w-[6px] rounded-full bg-nier-100 opacity-0" />
-        <span className="line h-[2px] w-0 bg-nier-100 opacity-0 transition-all duration-[30ms]" />
-        <span className="dot h-[6px] w-[6px] rounded-full bg-nier-100 opacity-0" />
+      <div className="MenuLine pointer-events-none flex items-center justify-center gap-1">
+        <span
+          className={`dot h-[6px] w-[6px] rounded-full bg-nier-100 
+          ${showLine ? 'opacity-100' : 'opacity-0'}`}
+        />
+        <span
+          className={`line h-[2px] bg-nier-100 transition-all duration-[30ms] 
+          ${showLine ? 'w-48 opacity-100 md:w-72' : 'w-0 opacity-0'}`}
+        />
+        <span
+          className={`dot h-[6px] w-[6px] rounded-full bg-nier-100 
+          ${showLine ? 'opacity-100' : 'opacity-0'}`}
+        />
       </div>
     </div>
   )
