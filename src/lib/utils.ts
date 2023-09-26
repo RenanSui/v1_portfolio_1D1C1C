6688db791854ed46ef708b65d04d4e0dc7e17e3a
RenanSui <renanddtao@gmail.com>
2023-09-26 13:25:32 -0300

    added CapitalizeWords function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -34,3 +34,14 @@ export const setRefAttribute = (
 ) => {
   ref?.setAttribute(attribute, String(value))
 }
+
+export const CapitalizeWords = (word: string) => {
+  const wordSplitted = word.trim().split(' ')
+  const joinWords = wordSplitted
+    .map((word) => {
+      return word[0]?.toLocaleUpperCase().concat(word.substring(1))
+    })
+    .join(' ')
+
+  return joinWords
+}
