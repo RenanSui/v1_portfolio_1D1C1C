df46ca01713f409819a9393594d3b0da1e0158dd
RenanSui <renanddtao@gmail.com>
2023-10-01 20:22:12 -0300

    better pattern2 and vignette

--- src/components/nier/nier-pattern2.tsx
+++ src/components/nier/nier-pattern2.tsx
@@ -1,5 +1,5 @@
 export const NierPattern2 = () => {
   return (
-    <div className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-50 bg-[url(/images/pattern.png)] bg-[length:5px_5px] bg-repeat opacity-[0.03]" />
+    <div className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-50 bg-[url(/images/pattern.png)] bg-[length:5px_5px] bg-repeat opacity-[0.07]" />
   )
 }
--- src/components/nier/nier-vignette.tsx
+++ src/components/nier/nier-vignette.tsx
@@ -1,5 +1,5 @@
 export const NierVignette = () => {
   return (
-    <div className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-50 bg-[radial-gradient(circle,_hsla(0,_0%,_100%,_0.05)_0%,_hsla(0,_0%,_0%,_0.2)_100%)]" />
+    <div className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-50 bg-[radial-gradient(circle,_hsla(0,_0%,_100%,_0.1)_0%,_hsla(0,_0%,_0%,_0.3)_100%)]" />
   )
 }
