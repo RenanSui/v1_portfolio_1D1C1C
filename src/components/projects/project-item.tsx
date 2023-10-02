6f49a21681f9255f7f549712b298f08c7fe9b1d7
RenanSui <renanddtao@gmail.com>
2023-10-02 02:57:54 -0300

    add select project by mouse

--- src/components/projects/project-item.tsx
+++ src/components/projects/project-item.tsx
@@ -1,15 +1,23 @@
-import { HTMLAttributes } from 'react'
+import { useSelectMouse } from '@/hooks/use-select-mouse'
+import { HTMLAttributes, useRef } from 'react'
 import { LoadingText } from '../loading/loading-text'
-import { NierSquare } from '../nier/nier-square'
 import { NierSelector } from '../nier/nier-selector'
+import { NierSquare } from '../nier/nier-square'
 
 interface ProjectItemProps extends HTMLAttributes<HTMLDivElement> {
   children: string
 }
 
 export const ProjectItem = ({ children }: ProjectItemProps) => {
+  const ProjectRef = useRef<HTMLDivElement>(null)
+
+  useSelectMouse(ProjectRef)
+
   return (
-    <div className="group relative flex h-[45px] w-full max-w-[770px] transition-all duration-300 hover:bg-nier-700 data-[active=true]:bg-nier-700">
+    <div
+      className="group relative flex h-[45px] w-full max-w-[770px] transition-all duration-300 hover:bg-nier-700 data-[active=true]:bg-nier-700"
+      ref={ProjectRef}
+    >
       <NierSelector className="-left-12 top-1/2 z-50 hidden -translate-y-1/2 md:block" />
 
       <h1 className="mx-3 flex items-center gap-3 group-hover:text-nier-600 group-data-[active=true]:text-nier-600 md:text-xl">
