2705e35af77e3cea7ea3a4670acc01b77f51d47b
RenanSui <renanddtao@gmail.com>
2023-10-03 11:17:54 -0300

    remove refs from shell

--- src/components/shells/animated-shell.tsx
+++ src/components/shells/animated-shell.tsx
@@ -1,33 +1,24 @@
 import { HTMLMotionProps, motion } from 'framer-motion'
-import { forwardRef } from 'react'
 
-const AnimatedShell = forwardRef(
-  (
-    {
-      children,
-      className,
-      initial,
-      animate,
-      exit,
-      ...props
-    }: HTMLMotionProps<'div'>,
-    ref,
-  ) => {
-    return (
-      <motion.section
-        className={className}
-        initial={initial || { opacity: 0 }}
-        animate={
-          animate || { opacity: 1, transition: { delay: 0.6, duration: 1 } }
-        }
-        exit={exit || { opacity: 0, transition: { duration: 0.3 } }}
-        {...props}
-      >
-        {children}
-      </motion.section>
-    )
-  },
-)
-AnimatedShell.displayName = 'AnimatedShell'
-
-export { AnimatedShell }
+export const AnimatedShell = ({
+  children,
+  className,
+  initial,
+  animate,
+  exit,
+  ...props
+}: HTMLMotionProps<'div'>) => {
+  return (
+    <motion.section
+      className={className}
+      initial={initial || { opacity: 0 }}
+      animate={
+        animate || { opacity: 1, transition: { delay: 0.6, duration: 1 } }
+      }
+      exit={exit || { opacity: 0, transition: { duration: 0.3 } }}
+      {...props}
+    >
+      {children}
+    </motion.section>
+  )
+}
--- src/components/shells/menu-shell.tsx
+++ src/components/shells/menu-shell.tsx
@@ -1,37 +1,36 @@
 'use client'
 import { useSelectKeyboard } from '@/hooks/use-select-keyboard'
 import { cn } from '@/lib/utils'
-import { HTMLAttributes, ReactNode, forwardRef, useRef } from 'react'
+import { HTMLAttributes, useRef } from 'react'
 
 interface MenuOptionsProps extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactNode
   layout?: 'default' | 'full' | 'centered'
 }
 
-const MenuShell = forwardRef<HTMLDivElement, MenuOptionsProps>(
-  ({ children, className, layout = 'default', ...props }, ref) => {
-    const MenuRef = useRef<HTMLDivElement>(null)
+export const MenuShell = ({
+  children,
+  className,
+  layout = 'default',
+  ...props
+}: MenuOptionsProps) => {
+  const MenuRef = useRef<HTMLDivElement>(null)
 
-    useSelectKeyboard(MenuRef)
+  useSelectKeyboard(MenuRef)
 
-    return (
-      <div
-        ref={MenuRef}
-        className={cn(
-          '',
-          layout === 'default' && '',
-          layout === 'full' && 'h-full w-full',
-          layout === 'centered' &&
-            'flex h-full w-full flex-col items-center justify-center',
-          className,
-        )}
-        {...props}
-      >
-        {children}
-      </div>
-    )
-  },
-)
-MenuShell.displayName = 'MenuShell'
-
-export { MenuShell }
+  return (
+    <div
+      ref={MenuRef}
+      className={cn(
+        '',
+        layout === 'default' && '',
+        layout === 'full' && 'h-full w-full',
+        layout === 'centered' &&
+          'flex h-full w-full flex-col items-center justify-center',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
