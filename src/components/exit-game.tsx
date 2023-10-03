0fa214aa8ab91af3cfae797cc10b74d0b463fa22
RenanSui <renanddtao@gmail.com>
2023-10-03 12:48:44 -0300

    ExitButton relative and add NierSelector

--- src/components/exit-game.tsx
+++ src/components/exit-game.tsx
@@ -2,6 +2,7 @@ import { ScreenStates } from '@/app/(lobby)/page'
 import { Dispatch, HTMLAttributes, SetStateAction } from 'react'
 import { LoadingText } from './loading/loading-text'
 import { OptionStates } from './main-menu'
+import { NierSelector } from './nier/nier-selector'
 import { NierSquare } from './nier/nier-square'
 
 interface ExitGameProps {
@@ -31,9 +32,10 @@ const ExitButton = ({
   ...props
 }: HTMLAttributes<HTMLButtonElement>) => (
   <button
-    className="transitin-all group flex cursor-default items-center gap-4 bg-nier-400 p-2 pl-3 pr-16 text-xl text-black duration-300 hover:bg-nier-700 hover:text-nier-600 hover:shadow-[_5px_5px_0px_0px_rgba(166,160,136,1)]"
+    className="transitin-all group relative flex cursor-default items-center gap-4 bg-nier-400 p-2 pl-3 pr-16 text-xl text-black duration-300 hover:bg-nier-700 hover:text-nier-600 hover:shadow-[_5px_5px_0px_0px_rgba(166,160,136,1)]"
     {...props}
   >
+    <NierSelector className="-left-12 group-hover:opacity-100" />
     <NierSquare className="h-[20px] w-[20px] cursor-default bg-nier-900 group-hover:bg-nier-600" />
     {children}
   </button>
