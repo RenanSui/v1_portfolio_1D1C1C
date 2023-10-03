0fc930918c84050722af7f5e78f05c8dad1c0f87
RenanSui <renanddtao@gmail.com>
2023-10-03 14:44:19 -0300

    fix MessageData import

--- src/lib/emailjs.ts
+++ src/lib/emailjs.ts
@@ -1,4 +1,4 @@
-import { MessageData } from '@/components/contact-me'
+import { MessageData } from '@/components/contact/contact-form'
 import emailjs from '@emailjs/browser'
 
 export const SendEmail = (data: MessageData) => {
