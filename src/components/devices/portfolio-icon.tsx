982c8006959875c6d66a6f44460c265461a527af
RenanSui <renanddtao@gmail.com>
2023-10-06 14:21:42 -0300

    add title to portfolio-icon

--- src/components/devices/portfolio-icon.tsx
+++ src/components/devices/portfolio-icon.tsx
@@ -16,6 +16,7 @@ export const PortfolioIcon = ({
         'flex h-16 w-16 cursor-pointer items-center justify-center rounded-xl border-2 border-nier-900 bg-nier-300 hover:scale-105',
         className,
       )}
+      title="portfolio"
       onClick={() => setScreenState('boot-screen')}
     >
       <div className="relative flex h-[55px] w-[55px] items-center justify-center rounded-full border-[6px] border-nier-900 bg-transparent after:h-10 after:w-10  after:rounded-full after:border-4 after:border-transparent ">
