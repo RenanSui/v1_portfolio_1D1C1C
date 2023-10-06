f2d16530c6e69a6d4e67a55d09d1830971c98116
RenanSui <renanddtao@gmail.com>
2023-10-06 15:37:21 -0300

    fix profile clipping on nierSuggestions

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -53,7 +53,7 @@ const AboutMe = ({ setOptionState }: AboutMeProps) => {
       </div>
 
       {aboutState === 0 && (
-        <ContentShell>
+        <ContentShell className="md:max-h-[45vh] xl:max-h-[55vh] 2xl:max-h-[60vh]">
           <Profile {...{ setOptionState }} />
         </ContentShell>
       )}
@@ -73,54 +73,6 @@ const AboutMe = ({ setOptionState }: AboutMeProps) => {
           <Sections {...{ setOptionState }} />
         </ContentShell>
       )}
-
-      {/* <ContentShell>
-        <NierLine />
-
-        <AnimatePresence>
-          {aboutState === 0 && <Profile {...{ setOptionState }} />}
-        </AnimatePresence>
-        <AnimatePresence>{aboutState === 1 && <Skills />}</AnimatePresence>
-        <AnimatePresence>
-          {aboutState === 2 && <Sections {...{ setOptionState }} />}
-        </AnimatePresence>
-
-        <div className="absolute left-0 top-1/2 w-full -translate-y-1/2 ">
-          <span className="bg-nier-900 py-3 pl-20 pr-4 text-nier-600">
-            Renan
-          </span>
-          <p className="mt-6 w-full bg-nier-900 px-8 py-8 text-center text-nier-600">
-            Hello! I&apos;m Renan, and I&apos;m a self-taught front-end web
-            developer.
-          </p>
-        </div>
-
-        <h2 className="text-lg">
-          Hello! I&apos;m Renan, and I&apos;m a self-taught front-end web
-          developer.
-        </h2>
-        <p>
-          I worked for several years doing manual jobs, but every minute and
-          second of free time I dedicated to studying programming.
-        </p>
-
-        <p>
-          My focus is to create interactive applications, with an elegant and
-          appealing design.
-        </p>
-
-        <div className="mt-12 flex flex-col gap-4 text-black">
-          <p className="ml-[7px]">Hi, my name is</p>
-          <p className="text-7xl text-nier-800">Renan Sui</p>
-          <p className="ml-1 text-3xl text-nier-700">
-            I build thing for the web
-          </p>
-          <p className="ml-[6px] text-nier-700">
-            I&apos;m a software engineer specializing in building (and
-            occasionally designing) exceptional digital experiences
-          </p>
-        </div>
-      </ContentShell> */}
 
       <NierSuggestions onClick={backToMenu}>
         Explore my portfolio
