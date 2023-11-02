ab7fea2b0ad6e30b0962a35f53ac0ba6249734b0
RenanSui <renanddtao@gmail.com>
2023-11-02 15:19:24 -0300

    update resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import { ContactButton } from '@/components/contact/contact-button'
 import { NierLine } from '@/components/nier/nier-line'
 import { NierPattern } from '@/components/nier/nier-pattern'
 import { NierSuggestions } from '@/components/nier/nier-suggestions'
 import { ContentShell } from '@/components/shells/content-shell'
 import { Header } from '@/components/ui/header'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
@@ -20,12 +22,18 @@ export default function Home() {
 
       <Header onClick={backToPage}>RESUME</Header>
 
-      <ContentShell className="flex gap-6">
+      <div className="mx-3 mb-6 flex gap-4 md:mx-12">
+        <Link href={'/pdf/resume_renan_neves.pdf'} target="_blank">
+          <ContactButton>Go to Resume PDF</ContactButton>
+        </Link>
+      </div>
+
+      <ContentShell className="flex gap-6 md:max-h-[45vh] xl:max-h-[55vh] 2xl:max-h-[60vh]">
         <NierLine />
 
         <section className="projects flex w-full flex-col items-center gap-4 overflow-y-scroll xs:items-start">
           <object
-            data="pdf/Profile.pdf"
+            data="pdf/resume_renan_neves.pdf"
             type="application/pdf"
             className="relative z-[60] h-screen w-full"
           ></object>
