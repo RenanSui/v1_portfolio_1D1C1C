b0bdfe9dcfc7877f454311fb5fb8903604239f72
RenanSui <renanddtao@gmail.com>
2024-01-24 12:36:59 -0300

    update nier-vignette

--- src/features/nier/components/nier-vignette.tsx
+++ src/features/nier/components/nier-vignette.tsx
@@ -1,5 +1,5 @@
 export const NierVignette = () => {
   return (
-    <div className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-50 bg-[radial-gradient(circle,_hsla(0,_0%,_100%,_0.1)_0%,_hsla(0,_0%,_0%,_0.3)_100%)]" />
+    <div className="vignette pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-[10000] h-full w-full select-none" />
   )
 }
