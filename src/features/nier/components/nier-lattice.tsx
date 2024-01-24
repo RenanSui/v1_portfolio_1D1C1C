413118563571bc962db426640478bf6800dd7549
RenanSui <renanddtao@gmail.com>
2024-01-24 10:38:27 -0300

    add nier-lattice component

---
+++ src/features/nier/components/nier-lattice.tsx
@@ -0,0 +1,5 @@
+export const NierLattice = () => {
+  return (
+    <div className="pointer-events-none fixed left-0 top-0 z-[10000] h-full w-full select-none bg-[url(/assets/wallpapers/Nie-R-Lattice.png)] opacity-[0.03]" />
+  )
+}
