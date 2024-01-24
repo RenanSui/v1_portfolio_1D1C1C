9d1bb4ca26d44a059fc33835d981413d3926445b
RenanSui <renanddtao@gmail.com>
2024-01-24 12:21:31 -0300

    change fonts folder, update font path

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,33 +1,33 @@
 import localFont from 'next/font/local'
 
 export const Concielian = localFont({
-  src: '../../public/fonts/concielianjetexpand.ttf',
+  src: '../../public/assets/fonts/concielianjetexpand.ttf',
 })
 
 export const RodinPro = localFont({
   src: [
     {
-      path: '../../public/fonts/FOT-Rodin Pro L.otf',
+      path: '../../public/assets/fonts/FOT-Rodin Pro L.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/FOT-Rodin Pro M.otf',
+      path: '../../public/assets/fonts/FOT-Rodin Pro M.otf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/FOT-Rodin Pro DB.otf',
+      path: '../../public/assets/fonts/FOT-Rodin Pro DB.otf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/FOT-Rodin Pro B.otf',
-      weight: '700',
+      path: '../../public/assets/fonts/FOT-Rodin Pro B.otf',
+      weight: 'bold',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/FOT-Rodin Pro UB.otf',
+      path: '../../public/assets/fonts/FOT-Rodin Pro UB.otf',
       weight: '800',
       style: 'normal',
     },
