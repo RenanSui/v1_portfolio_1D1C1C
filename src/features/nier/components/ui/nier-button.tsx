3795c6b407284fdb3c6ecf9b65fe601ed2eb6c19
RenanSui <renanddtao@gmail.com>
2024-01-26 12:24:35 -0300

    add active feature to nier-button

--- src/features/nier/components/ui/nier-button.tsx
+++ src/features/nier/components/ui/nier-button.tsx
@@ -5,12 +5,14 @@ import { HTMLAttributes } from 'react'
 interface NierButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: string
   reverse?: boolean
+  active?: boolean
 }
 
 export const NierButton = ({
   children,
   className,
   reverse,
+  active,
   ...props
 }: NierButtonProps) => {
   return (
@@ -18,6 +20,7 @@ export const NierButton = ({
       className={cn(
         'transitin-all group relative flex cursor-default items-center gap-4 bg-nier-light-trans-500 p-2 pl-3 pr-6 tracking-wider text-black duration-150 hover:bg-nier-light-800 hover:text-nier-light-100 hover:shadow-[_5px_5px_0px_0px_rgba(151,147,129,0.6)] md:pr-16 md:text-xl',
         className,
+        active ? 'animate-pulse bg-nier-light-800 text-nier-light-100' : '',
       )}
       {...props}
     >
@@ -25,9 +28,15 @@ export const NierButton = ({
         className={cn(
           'pointer-events-none hidden group-hover:opacity-100 md:block',
           reverse ? '-right-12 rotate-90' : '-left-12',
+          active ? 'opacity-100' : '',
         )}
       />
-      <NierSquare className="pointer-events-none h-[20px] w-[20px] cursor-default bg-nier-light-800 group-hover:bg-nier-light-100" />
+      <NierSquare
+        className={cn(
+          'pointer-events-none h-[20px] w-[20px] cursor-default bg-nier-light-800 group-hover:bg-nier-light-100',
+          active ? 'bg-nier-light-100' : '',
+        )}
+      />
       <NierLoadingText>{children}</NierLoadingText>
     </button>
   )
