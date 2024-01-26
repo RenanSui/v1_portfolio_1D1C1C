0697bbe9d4469642d4e3358906263509314c63fe
RenanSui <renanddtao@gmail.com>
2024-01-26 12:32:43 -0300

    better resume page design

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,11 @@
 'use client'
 
-import { NierButton, NierPattern } from '@/features/nier'
+import {
+  NierButton,
+  NierLine,
+  NierPattern,
+  NierVignette,
+} from '@/features/nier'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
@@ -10,22 +15,26 @@ export default function Home() {
 
   return (
     <main className="relative flex min-h-screen flex-col bg-nier-light-100">
-      <NierPattern variant="block" />
+      <div className="mx-3 my-4 flex h-full max-h-[40px] gap-6 md:mx-12 ">
+        <NierLine className="absolute h-[40px]" />
+        <div className="flex h-[40px] w-full max-w-full gap-[56px] overflow-hidden pl-16">
+          <NierButton className="tracking-widest" onClick={backToPage}>
+            Portfolio
+          </NierButton>
+        </div>
+      </div>
+
+      <NierVignette variant={'light'} />
+
+      <NierPattern variant="block" className="lg:py-0" />
 
       <h1 className="sr-only">Portfolio / CV</h1>
 
       <div className="flex flex-1 flex-col">
-        <NierButton
-          className="mx-2 flex self-start"
-          onClick={backToPage}
-          reverse
-        >
-          Back to Portfolio
-        </NierButton>
         <object
           data="pdf/resume_renan_neves_atualizado.pdf"
           type="application/pdf"
-          className="h-full w-full flex-1 p-2"
+          className="z-[10000] h-full w-full flex-1 p-2"
         />
       </div>
 
