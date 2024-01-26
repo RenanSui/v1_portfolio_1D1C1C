a9d151a0bc3d33e91d7de40f3302b44209be79ad
RenanSui <renanddtao@gmail.com>
2024-01-26 19:59:52 -0300

    faster menu-list animation

--- src/features/main-menu/components/menu-list.tsx
+++ src/features/main-menu/components/menu-list.tsx
@@ -18,11 +18,14 @@ export const MenuList = () => {
   }
 
   return (
-    <AnimatedShell className="absolute bottom-20 left-1/2 -translate-x-1/2 sm:bottom-40">
+    <AnimatedShell
+      className="absolute bottom-20 left-1/2 -translate-x-1/2 sm:bottom-40"
+      animate={{ opacity: 1, transition: { delay: 0, duration: 0.6 } }}
+    >
       <div data-elementtype="menu" className="flex flex-col gap-3">
         <MenuItem
           data-active="true"
-          index={0.5}
+          index={0}
           textHidden={'About Me'}
           onClick={() => handleMenuSections('about-me')}
         >
@@ -30,7 +33,7 @@ export const MenuList = () => {
         </MenuItem>
 
         <MenuItem
-          index={0.6}
+          index={0.1}
           textHidden={'Projects'}
           onClick={() => handleMenuSections('projects')}
         >
@@ -38,19 +41,19 @@ export const MenuList = () => {
         </MenuItem>
 
         <MenuItem
-          index={0.7}
+          index={0.2}
           textHidden={'Settings'}
           onClick={() => handleMenuSections('settings')}
         >
           Settings
         </MenuItem>
 
-        <MenuItem textHidden={'Resume'} index={0.6} onClick={goToResumePage}>
+        <MenuItem textHidden={'Resume'} index={0.3} onClick={goToResumePage}>
           Resume
         </MenuItem>
 
         <MenuItem
-          index={0.8}
+          index={0.4}
           textHidden={'Contact'}
           onClick={() => handleMenuSections('contact')}
         >
@@ -58,7 +61,7 @@ export const MenuList = () => {
         </MenuItem>
 
         <MenuItem
-          index={0.9}
+          index={0.5}
           textHidden={'Exit Game'}
           onClick={() => handleMenuSections('exit-game')}
         >
