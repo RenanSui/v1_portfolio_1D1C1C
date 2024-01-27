ceac6177fd53ba1bb9099c563ea98ff042eb28ce
RenanSui <renanddtao@gmail.com>
2024-01-27 09:16:11 -0300

    fix skills links

--- src/features/menu-sections/config/index.ts
+++ src/features/menu-sections/config/index.ts
@@ -76,8 +76,10 @@ export const skillItems: SkillItem[] = [
     id: 0,
     name: 'Programação de Computadores',
     organization: 'Udemy',
-    pdfLink: 'certificado_de_introducao_a_programacao_de_computadores.pdf',
-    imageLink: 'certificado_de_introducao_a_programacao_de_computadores.png',
+    pdfLink:
+      '/pdf/certificados/certificado_de_introducao_a_programacao_de_computadores.pdf',
+    imageLink:
+      '/images/certificados/certificado_de_introducao_a_programacao_de_computadores.png',
     bgImage:
       'bg-[url(/images/certificados/certificado_de_introducao_a_programacao_de_computadores.png)]',
   },
@@ -85,8 +87,10 @@ export const skillItems: SkillItem[] = [
     id: 1,
     name: 'Desenvolvimento Web Completo',
     organization: 'Udemy',
-    pdfLink: 'certificado_de_desenvolvimento_web_completo.pdf',
-    imageLink: 'certificado_de_desenvolvimento_web_completo.jpg',
+    pdfLink:
+      '/pdf/certificados/certificado_de_desenvolvimento_web_completo.pdf',
+    imageLink:
+      '/images/certificados/certificado_de_desenvolvimento_web_completo.jpg',
     bgImage:
       'bg-[url(/images/certificados/certificado_de_desenvolvimento_web_completo.jpg)]',
   },
@@ -94,8 +98,9 @@ export const skillItems: SkillItem[] = [
     id: 2,
     name: 'The Complete SQL Bootcamp',
     organization: 'Udemy',
-    pdfLink: 'certificado_de_the_complete_sql_bootcamp.pdf',
-    imageLink: 'certificado_de_the_complete_sql_bootcamp.jpg',
+    pdfLink: '/pdf/certificados/certificado_de_the_complete_sql_bootcamp.pdf',
+    imageLink:
+      '/images/certificados/certificado_de_the_complete_sql_bootcamp.jpg',
     bgImage:
       'bg-[url(/images/certificados/certificado_de_the_complete_sql_bootcamp.jpg)]',
   },
@@ -103,8 +108,10 @@ export const skillItems: SkillItem[] = [
     id: 3,
     name: 'Aprendendo C++ básico e avançado',
     organization: 'Udemy',
-    pdfLink: 'certificado_de_aprendendo_c++_basico_e_avancado.pdf',
-    imageLink: 'certificado_de_aprendendo_c++_basico_e_avancado.jpg',
+    pdfLink:
+      '/pdf/certificados/certificado_de_aprendendo_c++_basico_e_avancado.pdf',
+    imageLink:
+      '/images/certificados/certificado_de_aprendendo_c++_basico_e_avancado.jpg',
     bgImage:
       'bg-[url(/images/certificados/certificado_de_aprendendo_c++_basico_e_avancado.jpg)]',
   },
@@ -112,7 +119,8 @@ export const skillItems: SkillItem[] = [
     id: 4,
     name: 'Inglês & Informática básica',
     organization: 'Ibep formation',
-    imageLink: 'certificado_de_ingles_basico_e_informatica_basica.jpg',
+    imageLink:
+      '/images/certificados/certificado_de_ingles_basico_e_informatica_basica.jpg',
     bgImage:
       'bg-[url(/images/certificados/certificado_de_ingles_basico_e_informatica_basica.jpg)]',
   },
