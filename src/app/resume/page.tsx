d997d5d7ffb4b6c985df42ff733c3c7fb7b5790c
RenanSui <renanddtao@gmail.com>
2024-01-29 17:57:08 -0300

    add link to go back

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,18 +1,12 @@
-'use client'
-
 import {
   NierButton,
   NierLine,
   NierPattern,
   NierVignette,
 } from '@/features/nier'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function Home() {
-  const router = useRouter()
-
-  const backToPage = () => router.push('/')
-
   return (
     <main className="relative flex min-h-screen flex-col bg-nier-light-100">
       <div className="pointer-events-none absolute left-0 top-0 h-full w-full bg-[url(/assets/wallpapers/light-theme-1440.png)] bg-cover" />
@@ -20,9 +14,9 @@ export default function Home() {
       <div className="mx-3 my-4 flex h-full max-h-[40px] gap-6 md:mx-12 ">
         <NierLine className="absolute h-[40px]" />
         <div className="flex h-[40px] w-full max-w-full gap-[56px] overflow-hidden pl-16">
-          <NierButton className="tracking-widest" onClick={backToPage}>
-            Portfolio
-          </NierButton>
+          <Link href="/">
+            <NierButton className="tracking-widest">Portfolio</NierButton>
+          </Link>
         </div>
       </div>
 
