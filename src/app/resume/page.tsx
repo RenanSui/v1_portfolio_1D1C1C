5bd335576814e85d1e51dc3cce50f8c933586272
RenanSui <renanddtao@gmail.com>
2024-01-30 22:21:45 -0300

    update resume page design

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <div className="pointer-events-none absolute left-0 top-0 h-full w-full bg-[url(/assets/wallpapers/light-theme-1440.png)] bg-cover" />
 
       <div className="mx-3 my-4 flex h-full max-h-[40px] gap-6 md:mx-12 ">
-        <NierLine className="absolute h-[40px]" />
-        <div className="flex h-[40px] w-full max-w-full gap-[56px] overflow-hidden pl-16">
+        <NierLine className="absolute hidden h-[40px] md:flex" />
+        <div className="flex h-[40px] w-full max-w-full gap-[56px] overflow-hidden md:pl-16">
           <Link href="/">
             <NierButton className="tracking-widest">Portfolio</NierButton>
           </Link>
@@ -26,12 +26,15 @@ export default function Home() {
 
       <h1 className="sr-only">Portfolio / CV</h1>
 
-      <div className="flex flex-1 flex-col">
-        <object
-          data="pdf/resume_renan_neves_atualizado.pdf"
-          type="application/pdf"
-          className="z-[10000] h-full w-full flex-1 p-2"
-        />
+      <div className="mx-3 flex h-full flex-1 flex-wrap gap-6 pb-8 md:mx-12 md:max-h-[800px] md:flex-row-reverse lg:my-4">
+        <div className="flex flex-1 flex-col">
+          <object
+            data="pdf/resume_renan_neves_atualizado.pdf"
+            type="application/pdf"
+            className="z-[10000] h-full w-full flex-1 p-2"
+          />
+        </div>
+        <NierLine className="hidden md:flex" />
       </div>
 
       <NierPattern variant="block" />
